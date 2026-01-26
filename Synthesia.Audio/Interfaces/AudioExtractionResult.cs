aa45b1a67c71ffca5047ff6fff77c6d2c5f9b552
Tsebo <tsbletele@gmail.com>
2026-01-26 23:46:43 +0200

    Define audio note extraction contracts

---
+++ Synthesia.Audio/Interfaces/AudioExtractionResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Synthesia.Core.Models;
+
+namespace Synthesia.Audio.Interfaces;
+
+public sealed record AudioExtractionResult(
+    IReadOnlyList<NoteEvent> Notes,
+    double DurationSeconds,
+    string SourceFile
+);
---
+++ Synthesia.Audio/Interfaces/IAudioNoteExtractor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Synthesia.Core.Models;
+
+namespace Synthesia.Audio.Interfaces;
+
+public interface IAudioNoteExtractor
+{
+    Task<AudioExtractionResult> ExtractAsync(
+        string audioFilePath,
+        CancellationToken cancellationToken = default
+    );
+}
