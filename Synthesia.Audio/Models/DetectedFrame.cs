dc5e9c24927a7209497b5a24f976df1ac2de070a
Tsebo <tsbletele@gmail.com>
2026-02-04 13:42:35 +0200

    Add: detected frames class

---
+++ Synthesia.Audio/Models/DetectedFrame.cs
@@ -0,0 +1,14 @@
+﻿namespace Synthesia.Audio.Models
+{
+    public sealed class DetectedFrame
+    {
+        public double TimeSeconds { get; }
+        public double Frequency { get; }
+
+        public DetectedFrame(double timeSeconds, double frequency)
+        {
+            TimeSeconds = timeSeconds;
+            Frequency = frequency;
+        }
+    }
+}
